93e2fbbba31252f3fca5ac250f86f3ae452a1eac
aam198 <aam198@psu.edu>
2025-02-06 15:22:05 -0500

    Comments updated in JS file

--- js/stressCalc.js
+++ js/stressCalc.js
@@ -1,12 +1,12 @@
 function calculateScaleScore() {
   let totalScore = 0;
 
-  // Get all the checkbox buttons
+  // Get all the checkbox buttons on the page
   const checkBoxButtons = document.querySelectorAll('input[type="checkbox"]:checked');
 
-  // Iterate over each selected radio button
+  // Iterate over each checked checkbox button
   checkBoxButtons.forEach(checkBoxButton => {
-    // Add the value of the selected checkbox button to the total score
+    // Add the value of each selected checkbox button to the total score
     totalScore += parseInt(checkBoxButton.value, 10);
   });
 
