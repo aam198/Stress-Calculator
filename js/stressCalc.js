2897cf5cf92e6118e827665c100b120458a39835
aam198 <aam198@psu.edu>
2025-02-06 15:21:03 -0500

    Updated input type and JS file to reflect input change

--- js/stressCalc.js
+++ js/stressCalc.js
@@ -1,13 +1,13 @@
 function calculateScaleScore() {
   let totalScore = 0;
 
-  // Get all the radio buttons
-  const radioButtons = document.querySelectorAll('input[type="radio"]:checked');
+  // Get all the checkbox buttons
+  const checkBoxButtons = document.querySelectorAll('input[type="checkbox"]:checked');
 
   // Iterate over each selected radio button
-  radioButtons.forEach(radioButton => {
-    // Add the value of the selected radio button to the total score
-    totalScore += parseInt(radioButton.value, 10);
+  checkBoxButtons.forEach(checkBoxButton => {
+    // Add the value of the selected checkbox button to the total score
+    totalScore += parseInt(checkBoxButton.value, 10);
   });
 
   // Display the total score
